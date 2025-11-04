5cdcf2942e386f5110324751a5e3693312ab725e
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-11-04 16:39:04 -0300

    date.js-ready

--- scripts/date.js
+++ scripts/date.js
@@ -3,3 +3,4 @@ const year = date.getFullYear();
 document.querySelector("#currentyear").textContent = year;
 
 let lastModification = new Date;
+document.querySelector("#lastModified").textContent = lastModification.toLocaleString();
