e4cd61b50968ceca3842de5d98ff40888641a829
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-11-06 17:05:48 -0300

    prophets-section class added- css on progress

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -11,12 +11,15 @@ async function getProphetData() {
 
 const displayProphets = (prophets) => {
     prophets.forEach((prophet => {
-
+    
     let card = document.createElement('section');
+    card.classList.add('prophet');
     let fullName = document.createElement('h2');
     let portrait = document.createElement('img');
+    let birthday = document.createElement('p');
 
     fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+    birthday.innerHTML = `<strong>Date of Birth:</strong> ${prophet.birthdate}<br><strong>Birth Place:</strong> ${prophet.birthplace}`;
     portrait.setAttribute("src", prophet.imageurl);
     portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
     portrait.setAttribute("loading", "lazy");
@@ -24,8 +27,9 @@ const displayProphets = (prophets) => {
     portrait.setAttribute("height", "440");
 
     card.appendChild(fullName);
+    card.appendChild(birthday);
     card.appendChild(portrait);
-    
+
     cards.appendChild(card);
     }));
 }
