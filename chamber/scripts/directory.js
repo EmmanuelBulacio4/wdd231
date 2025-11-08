b26205f79dbd92a619c27745f428d98b39ee69ca
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2025-11-08 19:00:36 -0300

    w2-directory-reviewed

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -12,10 +12,10 @@ const displayMembers = (members) => {
         // Creo los elementos HTML para luego mostrarla en la web
         let card = document.createElement('section');
         card.classList.add('member');
-        let name = document.createElement('p');
+        let name = document.createElement('h3');
         let address = document.createElement('p');
         let phone = document.createElement('p');
-        let website = document.createElement('p');
+        let website = document.createElement('a');
         let image = document.createElement('img');
         // let level = document.createElement('p');
         // // let info = document.createElement('p');
@@ -24,7 +24,8 @@ const displayMembers = (members) => {
         name.textContent = `${member.name}`;
         address.textContent = `${member.addresses}`;
         phone.textContent = `${member.phone}`;
-        website.textContent = `${member.website}`;
+        // website.textContent = `${member.website}`;
+        website.textContent = "WebSite";
         image.setAttribute("src", "");
         image.setAttribute("alt", "Main image of the member");
         image.setAttribute("loading", "lazy");
@@ -34,11 +35,11 @@ const displayMembers = (members) => {
         // info.textContent = `${member.info}`;
 
         //Agrego los elementos anteriores a la section (line 15)
+        card.appendChild(image);
         card.appendChild(name);
         card.appendChild(address);
         card.appendChild(phone);
         card.appendChild(website);
-        card.appendChild(image);
 
         //Ahora la card la agrego al container que es la variable que tiene el div donde se mostraran los miembros
         container.appendChild(card);
@@ -46,3 +47,25 @@ const displayMembers = (members) => {
 }
 
 getMembers();
+
+//CHANGE BETWEEN GRID AND LIST
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+
+
+gridbutton.addEventListener("click", () => {
+	display.classList.add("grid");
+	display.classList.remove("list");
+    gridbutton.classList.add("active")
+    listbutton.classList.remove("active");
+});
+
+listbutton.addEventListener("click", () => {
+    display.classList.add("list");
+    display.classList.remove("grid");
+    gridbutton.classList.remove("active")
+    listbutton.classList.add("active");
+})
