9d2c7bf9a2df5bd2708cf80c028f5466bf243b6c
EmmanuelBulacio4 <emmanuelbulacio4@gmail.com>
2025-11-12 07:56:37 -0300

    w3

---
+++ scripts/weather.js
@@ -0,0 +1,5 @@
+const currentTEmp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('');
+
+const ur = 'https://api.openweathermap.org/data/2.5/_______';
