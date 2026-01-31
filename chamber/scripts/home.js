f75bc2f9640e7a9906cdf462ddabd33c34bf1158
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2026-01-31 15:48:15 -0300

    hero-image.REady

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -75,3 +75,47 @@ forecastWeather(saltaFore);
 
 //-------------- THREE RAMDON MEMBERS Level 2 or 3------------------
 
+const container = document.querySelector('#spot-members');
+
+async function getMembers(){
+    const response = await fetch('data/members.json');
+    const members = await response.json();
+    displayMembers(members.directory);
+}
+
+const displayMembers = (members) => {
+    members.forEach(member => {    //hago un forEach para iterar por cada miembro
+
+        // Creo los elementos HTML para luego mostrarla en la web
+        let card = document.createElement('section');
+        card.classList.add('member');
+        let name = document.createElement('h3');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a');
+        let image = document.createElement('img');
+
+        //Asigno los valores que tiene cada miembro a las etiquetas anteriores
+        name.textContent = `${member.name}`;
+        address.textContent = `${member.addresses}`;
+        phone.textContent = `${member.phone}`;
+        website.textContent = "WebSite";
+        image.setAttribute("src", member.image);
+        image.setAttribute("alt", "Main image of the member");
+        image.setAttribute("loading", "lazy");
+        // image.setAttribute("width", "300");
+        // image.setAttribute("height", "200");
+
+        //Agrego los elementos anteriores a la section (line 15)
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+
+        //Ahora la card la agrego al container que es la variable que tiene el div donde se mostraran los miembros
+        container.appendChild(card);
+    });
+}
+
+getMembers();
