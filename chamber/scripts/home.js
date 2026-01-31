dd7a1bd4ff2899be34145b88c3168ac386d1617f
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2026-01-30 21:55:06 -0300

    forecast-working!

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -1,6 +1,6 @@
 //HTML ELEMENT SELECTION
 const myTown = document.querySelector('#town');
-const myGraphic = document.querySelector('#graphic'); 
+const myGraphic = document.querySelector('#graphic');
 const myDescription = document.querySelector('#description');
 const myTemperature = document.querySelector('#temperature');
 
@@ -42,35 +42,40 @@ function displayResults(data) {
 
 
 // para mostrar 3 miembros de nivel plata u oro hay que
-const saltaFore = `https://api.openweathermap.org/data/2.5/forecast?lat=-24.79&lon=-65.44&appid=&appid=902647b55a8f06d112512d2e9c5051f8&units=metric`;
-
-const one = document.getElementById('one');
-const two = document.getElementById('two');
-const three = document.getElementById('three');
+const saltaFore = `https://api.openweathermap.org/data/2.5/forecast?lat=-24.79&lon=-65.44&appid=902647b55a8f06d112512d2e9c5051f8&units=metric`;
 
 
 async function forecastWeather(saltaFore) {
-    try{
+    try {
         const response = await fetch(saltaFore);
         if (response.ok) {
             const dataFore = await response.json();
-            console.log(dataFore);
             DisplayForecast(dataFore);
         }
-        else{
+        else {
             throw error(await response.text());
         }
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
 }
 
+function DisplayForecast(dataFore) {
+    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const d = new Date();
+    const todayIndex = d.getDay();
+
+    const done = document.getElementById('done');
+    done.innerHTML = `<strong>${dayNames[(todayIndex + 1) % 7]}:</strong> ${Math.round(dataFore.list[8].main.temp)}°C`;
+
+    const dtwo = document.getElementById('dtwo');
+    dtwo.innerHTML = `<strong>${dayNames[(todayIndex + 2) % 7]}:</strong> ${Math.round(dataFore.list[16].main.temp)}°C`;
+
+    const dthree = document.getElementById('dthree');
+    dthree.innerHTML = `<strong>${dayNames[(todayIndex + 3) % 7]}:</strong> ${Math.round(dataFore.list[24].main.temp)}°C`;
+}
+
 forecastWeather(saltaFore);
 
-function DisplayForecast(dataFore) {
-    // myTown.innerHTML = data.name;
-    one.textContent = dataFore.daily[1].temp.day;
-    two.textContent = dataFore.daily[2].temp.day;
-    three.textContent = dataFore.daily[3].temp.day;
-    
-}
+
+
