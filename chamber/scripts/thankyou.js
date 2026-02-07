56f3fcb2db7939852eb63b0d0e004147eafcf078
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2026-02-07 11:39:41 -0300

    thankyou.html ready

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,14 +1,12 @@
-const date = new Date;
-const year = date.getFullYear();
-document.querySelector("#currentyear").textContent = year;
-
-let lastModification = new Date;
-document.querySelector("#lastModified").textContent = lastModification.toLocaleString();
 
 
 const newMember = new URLSearchParams(window.location.search);
 
+
+
 document.querySelector('#message').innerHTML = `
-    <p>Application submittion from: ${newMember.get('name')} ${newMember.get('surname')}</p>
-    <p>The ${newMember.get('title')} of ${newMember.get('description')}</p>
-`
+    <p>Application submission from ${newMember.get('name')} ${newMember.get('surname')}</p>
+    <p>The ${newMember.get('title')} of ${newMember.get('organization')}</p>
+    <p> Phone: ${newMember.get('phone')} </p>
+    <p>Email: ${newMember.get('email')}</p>
+    <p>Business Description: ${newMember.get('description')}</p>`
